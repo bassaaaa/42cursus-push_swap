b5faa3bdc54c23009805ad54a93cc6a073d5bfac
ksaotome <ksaotome@student.42tokyo.jp>
2026-05-09 16:55:27 +0900

    opereation_swap追加

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsito <tsito@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: ksaotome <ksaotome@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 17:35:51 by tsito             #+#    #+#             */
-/*   Updated: 2026/05/09 15:57:36 by tsito            ###   ########.fr       */
+/*   Updated: 2026/05/09 16:52:58 by ksaotome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,8 @@ char				**join_and_split(int ac, char **av);
 long				ft_strtol(const char *str);
 int					is_valid_int_format(char *str);
 int					parse_args(char **args, t_input *input);
+void				sa(t_stack *a);
+void				sb(t_stack *b);
+void				ss(t_stack *a, t_stack *b);
 
 #endif
