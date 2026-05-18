4c0dea57bb2533fdd345a090b70c0151cf1a3707
bassaaaa <white.galaxys2.album2@gmail.com>
2026-05-19 00:17:04 +0900

    オプション制限処理を追加

--- parse_options.c
+++ parse_options.c
@@ -6,7 +6,7 @@
 /*   By: tsito <tsito@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 00:00:00 by tsito             #+#    #+#             */
-/*   Updated: 2026/05/16 00:00:00 by tsito            ###   ########.fr       */
+/*   Updated: 2026/05/19 00:10:39 by tsito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,48 @@ static t_option	get_option(char *str)
 	return (OPTION_INVALID);
 }
 
-int	parse_options(char **args, t_input *input, size_t *i)
+static int	set_bench(t_input *input, int *has_bench)
+{
+	if (*has_bench)
+		return (0);
+	input->bench = 1;
+	*has_bench = 1;
+	return (1);
+}
+
+static int	set_strategy_option(char *str, t_input *input, int *has_strategy)
 {
 	t_option	option;
+
+	if (*has_strategy)
+		return (0);
+	option = get_option(str);
+	if (option == OPTION_INVALID)
+		return (0);
+	input->option = option;
+	*has_strategy = 1;
+	return (1);
+}
+
+int	parse_options(char **args, t_input *input, size_t *i)
+{
+	int	has_bench;
+	int	has_strategy;
 
 	*i = 0;
 	input->option = OPTION_ADAPTIVE;
 	input->bench = 0;
+	has_bench = 0;
+	has_strategy = 0;
 	while (args[*i] && ft_strncmp(args[*i], "--", 2) == 0)
 	{
 		if (ft_strncmp(args[*i], "--bench", ft_strlen("--bench") + 1) == 0)
-			input->bench = 1;
-		else
 		{
-			option = get_option(args[*i]);
-			if (option == OPTION_INVALID)
+			if (!set_bench(input, &has_bench))
 				return (0);
-			input->option = option;
 		}
+		else if (!set_strategy_option(args[*i], input, &has_strategy))
+			return (0);
 		(*i)++;
 	}
 	return (1);
